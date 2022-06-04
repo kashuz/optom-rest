1cfac0a7a35667b97db5c10faac0c46935f98c92
Kamil Rafikov <mailbox@kamil-rafik.com>
2022-06-04 23:15:57 +0300

    removed unused sql from installation script

--- sql/install.php
+++ sql/install.php
@@ -11,15 +11,6 @@
 
 $sql = array();
 
-$sql[] = 'CREATE TABLE IF NOT EXISTS `' . _DB_PREFIX_ . 'binshopsrest_reset_pass_tokens` (
-    `id_pass_tokens` int(11) NOT NULL AUTO_INCREMENT,
-    `reset_password_token` varchar(255) NOT NULL,
-    `reset_password_validity` varchar(255) NOT NULL,
-    `id_customer` int(11) NOT NULL,
-    `last_token_gen` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY  (`id_pass_tokens`)
-) ENGINE=' . _MYSQL_ENGINE_ . ' DEFAULT CHARSET=utf8;';
-
 foreach ($sql as $query) {
     if (Db::getInstance()->execute($query) == false) {
         return false;
